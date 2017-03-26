67eeacc6d1bc23c1531aef05ef9e126a478584fe
dT <dTNL.Homey@gmail.com>
2017-03-26 14:56:02 +0200

    Revert back to old report_parser check + removed callback

--- drivers/powernode-6/driver.js
+++ drivers/powernode-6/driver.js
@@ -11,7 +11,6 @@ module.exports = new ZwaveDriver(path.basename(__dirname), {
 		onoff: {
 			command_class: 'COMMAND_CLASS_SWITCH_BINARY',
 			command_get: 'SWITCH_BINARY_GET',
-			command_get_cb: false,
 			command_set: 'SWITCH_BINARY_SET',
 			command_set_parser: value => ({
 				'Switch Value': value,
@@ -32,7 +31,9 @@ module.exports = new ZwaveDriver(path.basename(__dirname), {
 			}),
 			command_report: 'METER_REPORT',
 			command_report_parser: report => {
-				if (typeof report.Properties2.Scale !== 'undefined' &&
+				if (report &&
+					report.hasOwnProperty('Properties2') &&
+					report.Properties2.hasOwnProperty('Scale') &&
 					report.Properties2.Scale === 2) {
 					return report['Meter Value (Parsed)'];
 				}
@@ -52,7 +53,9 @@ module.exports = new ZwaveDriver(path.basename(__dirname), {
 			}),
 			command_report: 'METER_REPORT',
 			command_report_parser: report => {
-				if (typeof report.Properties2.Scale !== 'undefined' &&
+				if (report &&
+					report.hasOwnProperty('Properties2') &&
+					report.Properties2.hasOwnProperty('Scale') &&
 					report.Properties2.Scale === 0) {
 					return report['Meter Value (Parsed)'];
 				}
